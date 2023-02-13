e84fcb435aa78045890270321c8b8301f6cca0cb
Gisela Fredes <giserofredes79@gmail.com>
2023-02-13 00:47:06 -0300

    Primer archivo subido al git

---
+++ js/funcionalidades.js
@@ -0,0 +1,23 @@
+// alert("Hola estoy despues del body");
+//No lo uyilizamos mucho
+console.log("Estas accediendo a código secreto");
+console.warn("Estas accediendo a código secreto");
+console,console.error("Estas accediendo a código secreto");
+
+//Como se define una variable en javascript
+//Hay dos formas
+//var miVariable=2;
+let miVariable1=2;
+//Definir constantes
+const pi=3.14
+
+console.log(miVariable1);
+console.log(typeof miVariable1);
+
+miVariable1='Pepe'
+console.log(miVariable1);
+console.log(typeof miVariable1);
+//pi=10;Larga error porque no le puedo cambiar el valor
+//pront sirve para ingresar valores
+let valorIngresadoEntero= parseInt(prompt('Ingrese un valor'));
+let valorIngresadoFloat= parseFloat(prompt('Ingrese un valor'));
